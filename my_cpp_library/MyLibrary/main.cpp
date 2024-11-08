42090f41d7c250e0fe85551eff941b50f39bee93
hawlang <smdong97@gmail.com>
2024-11-08 15:58:44 +0900

    Update main.cpp
    test를 위한 코드 작성

--- my_cpp_library/MyLibrary/main.cpp
+++ my_cpp_library/MyLibrary/main.cpp
@@ -1,3 +1,13 @@
-﻿int main() {
+#include "MyArray.h"
 
+int main() { 
+  MyArray<int, 10> test;
+  for (int i = 0; i < test.size(); ++i) {
+    test[i] = i;
+  }
+
+  for (int i = 0; i < test.size(); ++i) {
+    std::cout << test[i] << " ";
+  }
+  std::cout << '\n';
 }
